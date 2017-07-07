038a77fb6cdd00fbd4a385c8e449ca81fc461cbf
xinlei.yang <yangxinlei007@live.com>
2017-07-07 16:31:33 +0800

    添加宇宙飞船

--- OhAR/ViewController.m
+++ OhAR/ViewController.m
@@ -16,8 +16,8 @@ typedef enum : NSUInteger {
 @interface ViewController () <ARSCNViewDelegate>
 
 @property (nonatomic, strong) IBOutlet ARSCNView *sceneView;
-
 @property (nonatomic, strong) SCNNode *earthNode;
+@property (nonatomic, strong) SCNNode *shipRotationNode;
 
 @end
 
@@ -32,14 +32,54 @@ typedef enum : NSUInteger {
     
     // Show statistics such as fps and timing information
     self.sceneView.showsStatistics = YES;
+//    self.sceneView.debugOptions = ARSCNDebugOptionShowWorldOrigin | ARSCNDebugOptionShowFeaturePoints;
     
     // Create a new scene
     SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/earth.DAE"];
     self.earthNode = [[scene rootNode] childNodeWithName:@"earth" recursively:YES];
-    [self.earthNode setPosition:SCNVector3Make(0, 0, -1)];
+    [self.earthNode setPosition:SCNVector3Make(0, 0, -0.5)];
+    
+    SCNScene *shipScene = [SCNScene sceneNamed:@"art.scnassets/ship.scn"];
+    SCNNode *shipNode = [[shipScene rootNode] childNodeWithName:@"ship" recursively:YES];
+    [shipNode setPosition:SCNVector3Make(0, 0, -0.5)];
+    [shipNode setRotation:SCNVector4Make(1, 0, 0, -M_PI_2)];
+    [shipNode setScale:SCNVector3Make(0.1, 0.1, 0.1)];
+    
+    SCNParticleSystem *reactorParticle = [SCNParticleSystem particleSystemNamed:@"shipReactor.scnp" inDirectory:nil];
+    [reactorParticle setBirthRate:140];
+    [reactorParticle setParticleSize:0.005];
+    [reactorParticle setParticleLifeSpan:0.09];
+    [reactorParticle setParticleVelocity:0.13];
+    SCNSphere *reactorGeo = [SCNSphere sphereWithRadius:0];
+    
+    SCNNode *leftReactorNode = [SCNNode nodeWithGeometry:reactorGeo];
+    [leftReactorNode setPosition:SCNVector3Make(-0.03, 0.012, -0.16)];
+    [leftReactorNode setRotation:SCNVector4Make(1, 0, 0, -M_PI_2)];
+    [leftReactorNode addParticleSystem:reactorParticle];
+    [shipNode addChildNode:leftReactorNode];
+    
+    SCNNode *rightReactorNode = [SCNNode nodeWithGeometry:reactorGeo];
+    [rightReactorNode setPosition:SCNVector3Make(0.03, 0.012, -0.16)];
+    [rightReactorNode setRotation:SCNVector4Make(1, 0, 0, -M_PI_2)];
+    [rightReactorNode addParticleSystem:reactorParticle];
+    [shipNode addChildNode:rightReactorNode];
+    
+    
+    
+    SCNSphere *sphereGeometry = [SCNSphere sphereWithRadius:0.01];
+    self.shipRotationNode = [SCNNode nodeWithGeometry:sphereGeometry];
+    [self.shipRotationNode setPosition:SCNVector3Make(0, 0, -0.5)];
+    [self.shipRotationNode addChildNode:shipNode];
+    [[scene rootNode] addChildNode:self.shipRotationNode];
     
     // Set the scene to the view
     self.sceneView.scene = scene;
+    
+    // 持续旋转shipRotationNode
+    SCNAction *rAction = [SCNAction rotateByAngle:M_PI * 2 aroundAxis:SCNVector3Make(1, 0, 0) duration:20];
+    SCNAction *kAction = [SCNAction repeatActionForever:rAction];
+    [self.shipRotationNode runAction:kAction];
+    
     
     // 处理拖动
     UIPanGestureRecognizer *panRecoginzer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panned:)];
@@ -66,7 +106,7 @@ typedef enum : NSUInteger {
     // Create a session configuration
     ARWorldTrackingSessionConfiguration *configuration = [ARWorldTrackingSessionConfiguration new];
     
-//    [configuration setPlaneDetection:ARPlaneDetectionHorizontal];
+    [configuration setPlaneDetection:ARPlaneDetectionHorizontal];
     // Run the view's session
     [self.sceneView.session runWithConfiguration:configuration];
 }
@@ -155,6 +195,7 @@ typedef enum : NSUInteger {
         SCNVector3 hitPointPosition = SCNVector3Make(hitPointTransform.m41, hitPointTransform.m42, hitPointTransform.m43);
         
         [self.earthNode setPosition:hitPointPosition];
+        [self.shipRotationNode setPosition:hitPointPosition];
     }
 }
 
@@ -167,7 +208,9 @@ typedef enum : NSUInteger {
         prevTranslatedX = translatedPoint.x;
     }
     
-    [self.earthNode setRotation:SCNVector4Make(0, 1, 0, self.earthNode.rotation.w + (translatedPoint.x - prevTranslatedX) / 12.0)];
+    SCNVector4 rotateVector = SCNVector4Make(0, 1, 0, self.earthNode.rotation.w + (translatedPoint.x - prevTranslatedX) / 12.0);
+    [self.earthNode setRotation:rotateVector];
+    [self.shipRotationNode setRotation:rotateVector];
     prevTranslatedX = translatedPoint.x;
 }
 
